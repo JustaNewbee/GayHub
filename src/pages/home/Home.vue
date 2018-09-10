8bbd4b570403ac20f6493473288947d5fecf5f8b
john <john@192.168.2.107>
2018-09-10 23:49:49 +0800

    init

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="home-page">
+        this is home page~
+        <button @click="changeTheme('')">换默认</button>
+        <button @click="changeTheme('1')">换红肤</button>
+        <button @click="changeTheme('2')">换蓝肤</button>
+        <button @click="changeTheme('3')">换绿肤</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+    methods: {
+        changeTheme (theme) {
+            window.document.documentElement.setAttribute('data-theme', theme)
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.home-page {
+    @include textColor();
+}
+</style>
